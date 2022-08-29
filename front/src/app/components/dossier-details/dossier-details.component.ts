887b414ba4f1f5e2004cb0b8568a3b8e59f88b06
Hannachi Houssem <houssem.hannachi@enis.tn>
2022-08-29 14:05:59 +0100

    Dossier Details

--- front/src/app/components/dossier-details/dossier-details.component.ts
+++ front/src/app/components/dossier-details/dossier-details.component.ts
@@ -12,7 +12,9 @@ export class DossierDetailsComponent implements OnInit {
 
   id: number | undefined;
   dossier: any;
-  entretiens: any=[];
+  entretiens: any = [];
+  entretienValide: any;
+  entretienAttente: any;
   test: any;
 
   constructor(private dossierService: DossierService, private route: ActivatedRoute) {
@@ -24,10 +26,34 @@ export class DossierDetailsComponent implements OnInit {
     this.dossier = new DossierCandidature();
     this.dossierService.getDossierById(this.id).subscribe(data => {
       this.dossier = data;
-      this.dossier.entretiens.forEach((e:any)=>{
-        this.entretiens=this.entretiens.concat(e.state)
+      this.dossier.entretiens.forEach((e: any) => {
+        this.entretiens = this.entretiens.concat(e.state)
       })
+      if (this.dossier.entretiens.length > 0) {
+        this.entretienAttente = "En attente";
+      }
       this.test = this.entretiens.includes('VALIDE');
+
+      if (this.entretiens.includes('VALIDE')) {
+        this.entretienValide = "Entretien planifié et validé"
+      } else if (this.entretiens.includes('EN_ATTENTE')) {
+        this.entretienValide = "En attente de validation de manager"
+      } else {
+        this.entretienValide = "En cours";
+      }
+      this.entretiens.forEach((e: any) => {
+        if (e == 'REFUSE') {
+          this.entretienAttente = "Proposition d'entretien refusé par le manager";
+        } else {
+          this.entretienAttente = "En attente";
+        }
+      })
+
+      if (this.dossier.entretiens.length == 0) {
+        this.entretienAttente = "Pas d'entretien planifié";
+      }
+
+
     });
   }
 
