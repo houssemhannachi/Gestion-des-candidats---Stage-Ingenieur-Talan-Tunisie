3d6f56319d26103559f0da0c4110206da4c680be
nawres chouari <nawres.chouari@esprit.tn>
2022-08-30 21:17:09 +0100

    state

--- front/src/app/_pipes/search-manager.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SearchManagerPipe } from './search-manager.pipe';
-
-describe('SearchManagerPipe', () => {
-  it('create an instance', () => {
-    const pipe = new SearchManagerPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
